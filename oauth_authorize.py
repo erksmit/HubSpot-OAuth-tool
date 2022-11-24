66df5af2e7c9a87c04f387be182f8c421514e032
Erik Smit <erik@ibabs.dev>
2022-11-24 14:09:01 +0100

    add optional scopes

--- oauth_authorize.py
+++ oauth_authorize.py
@@ -11,11 +11,8 @@ config = json5.load(open('config.json5'))
 client_id = config['client_id']
 client_secret = config['client_secret']
 redirect_uri = 'http://localhost:5000/oauth_callback'
-scopes = config['scopes']
-refresh_token = config['refresh_token']
-use_oauth = config['use_oauth']
-access_token = config['access_token']
-
+scopes: list = config['scopes']
+optional_scopes: list = config['optional_scopes']
 
 flask = Flask(__name__)
 
@@ -42,8 +39,11 @@ def exchange_tokens(auth_code: str):
         print(response.text)
         return response.text
 
-
 scope_string = '%20'.join(scopes)
 auth_url = f'https://app.hubspot.com/oauth/authorize?client_id={client_id}&scope={scope_string}&redirect_uri={redirect_uri}'
+
+if optional_scopes.count() > 0:
+    optional_scope_string = '%20'.join(optional_scopes)
+    auth_url += f'&optional_scope={optional_scope_string}'
 
 webbrowser.open(auth_url)
